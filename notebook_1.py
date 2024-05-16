eafd1cc65107911e858dfa251f3a7d1d14e205ac
elmunye <elyasmunye@gmail.com>
2024-05-16 11:15:56 +0200

    put online

---
+++ notebook_1.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.write("Hello, world!")
+
+#include header in the app
+st.header("My first app")
+
+#include a subtitle in the app
+st.title("NBA Playoffs hightlights:")
+
+#include a text in the app
+st.write("The Denver Nuggets made an impressive come back against the Minnesota Timberwolves in the NBA playoffs.")
+
+#include a youtube video in the app
+st.video("https://www.youtube.com/watch?v=BUVMqMsgVqU")
+
+
